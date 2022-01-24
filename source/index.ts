0109c1f1d29434ad8c27def40159e5e5278fe8b0
bakoe <ba.koe96@gmail.com>
2022-01-24 18:54:15 +0100

    Migrate Buffer, Geometry, and VertexArray

--- source/index.ts
+++ source/index.ts
@@ -11,7 +11,10 @@ export { ContextMasquerade } from './contextmasquerade';
 export { ExtensionsHash } from './extensionshash';
 export { ChangeLookup } from './changelookup';
 
+export { Buffer } from './buffer';
+export { Geometry } from './geometry';
 export { Texture2D } from './texture2d';
+export { VertexArray } from './vertexarray';
 export { Wizard } from './wizard';
 
 /* spellchecker: enable */
